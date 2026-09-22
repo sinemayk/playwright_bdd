a3bc959b344e6d917508dfc6f6e46e568a8dc9c2
Sinem <sinemayk@gmail.com>
2026-09-22 07:16:55 +0200

    env

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 import { defineBddConfig } from "playwright-bdd";
  import dotenv from "dotenv";
  import path from 'path';
- //dotenv.config({ path: path.resolve(__dirname, '.env') });
+ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const testDir = defineBddConfig({
   paths: ["e2e/features/**/*.feature"],
